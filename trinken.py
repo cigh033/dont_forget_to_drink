a6862eb069c1fc2d4c793b3e1e452e6bf3bd8ab0
SB <mail@cigh.de>
2019-04-01 15:16:38 +0200

    added supress window when powerpoint is opened (via powershell subprocess)

--- trinken.py
+++ trinken.py
@@ -1,9 +1,21 @@
 import tkinter
 from tkinter import messagebox
+import subprocess
+
+startupinfo = None
+
+startupinfo = subprocess.STARTUPINFO()
+startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+proc = subprocess.run("powershell -command $ProcessActive = Get-Process powerpnt -ErrorAction SilentlyContinue;if($ProcessActive -eq $null){exit 1}else{exit 2}",startupinfo=startupinfo)
+ 
 
 # hide main window
 root = tkinter.Tk()
 root.withdraw()
 root.attributes("-topmost", True)
 
-messagebox.showwarning("Trinken","Trinken nicht vergessen!")
+
+# message box display
+if proc.returncode == 1:
+	messagebox.showwarning("Trinken","Trinken nicht vergessen!")
