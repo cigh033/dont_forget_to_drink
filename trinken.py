919dbb80b9df2d93e8f82170a8e62e62189f1bc5
SB <mail@cigh.de>
2019-10-18 14:43:35 +0200

    removed ugly subprocess powershell call to check if presentation software is running

--- trinken.py
+++ trinken.py
@@ -1,21 +1,21 @@
 import tkinter
 from tkinter import messagebox
-import subprocess
+import psutil
 
-startupinfo = None
-
-startupinfo = subprocess.STARTUPINFO()
-startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-
-proc = subprocess.run("powershell -command $ProcessActive = Get-Process powerpnt -ErrorAction SilentlyContinue;if($ProcessActive -eq $null){exit 1}else{exit 2}",startupinfo=startupinfo)
- 
+for proc in psutil.process_iter():
+	if proc.name().lower() == "powerpnt.exe":
+		hidden = True
+		break
+	else:
+		hidden = False
 
 # hide main window
 root = tkinter.Tk()
 root.withdraw()
-root.attributes("-topmost", True)
 
-
-# message box display
-if proc.returncode == 1:
-	messagebox.showwarning("Trinken","Trinken nicht vergessen!")
+if hidden == False:
+	root.attributes("-topmost", True)
+else:
+	root.attributes("-topmost", False)
+	
+messagebox.showwarning("Trinken","Trinken nicht vergessen!")
