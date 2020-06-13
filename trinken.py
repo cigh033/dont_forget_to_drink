3f2c78a254d59844487668d26f66ff568c11e611
SB <mail@cigh.de>
2020-06-13 13:20:01 +0200

    changed if routine

--- trinken.py
+++ trinken.py
@@ -2,12 +2,12 @@ import tkinter
 from tkinter import messagebox
 import psutil
 
-for proc in psutil.process_iter():
-	if proc.name().lower() == "powerpnt.exe":
-		hidden = True
+for proc in psutil.process_iter(['name']):
+	if not proc.name().lower() == "powerpnt.exe":
+		hidden = False
 		break
 	else:
-		hidden = False
+		hidden = True
 
 # hide main window
 root = tkinter.Tk()
@@ -17,5 +17,5 @@ if hidden == False:
 	root.attributes("-topmost", True)
 else:
 	root.attributes("-topmost", False)
-	
+
 messagebox.showwarning("Trinken","Trinken nicht vergessen!")
